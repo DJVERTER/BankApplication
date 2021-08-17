1f0317d242f29b244b4f3d39a801b3937c887ebe
DJVERTER <62717168+DJVERTER@users.noreply.github.com>
2021-08-17 11:24:21 +0300

    Added New "!"

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -6,7 +6,7 @@ namespace BankApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
